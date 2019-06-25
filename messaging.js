5ab5d045e6cf871ea83d94cb3aa5dd69eeb44407
FeliOdras <ariane.feldmann@odras.de>
2019-06-25 10:09:31 +0200

    Added text input and output

--- messaging.js
+++ messaging.js
@@ -1,1 +1,7 @@
-const showHelp = () => {};
+const showHelp = () => {
+  // handle input
+  // no input to see here
+  const helpText = "Here the help text appears";
+  // print output
+  console.log(helpText);
+};
