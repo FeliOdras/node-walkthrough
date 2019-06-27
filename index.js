b6c58cfd6c49741337c5b30832cbee5ad8415eb8
FeliOdras <ariane.feldmann@odras.de>
2019-06-27 11:07:43 +0200

    Added condition for error handling

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const { getTimeData } = require("./lib/client");
 
 if (args.includes("--help")) {
   showHelp();
-}
-
-if (args.includes("--option")) {
+} else if (args.includes("--option")) {
   getTimeData()
     .then(data => {
       return data.reduce((acc, curr) => {
@@ -21,35 +19,38 @@ if (args.includes("--option")) {
         console.log(entry);
       });
       process.exit();
+    })
+    .catch(err => {
+      console.log(`Something went wrong. \n ${err}`);
     });
+} else {
+  getTimeData(args[0], args[1]).then(data => {
+    switch (args[2]) {
+      case "--unix":
+        return console.log(
+          `Current unix timestamp for ${prepareString(args[1])}/${prepareString(
+            args[0]
+          )} is ${data["unixtime"]}`
+        );
+      case "--currentWeek":
+        return console.log(
+          `It is week ${data["week_number"]} in ${prepareString(
+            args[1]
+          )}/${prepareString(args[0])}.`
+        );
+      case "--dayOfTheYear":
+        return console.log(
+          `It is day ${data["day_of_year"]} of the year in ${prepareString(
+            args[1]
+          )}/${prepareString(args[0])}.`
+        );
+      default:
+        return console.log(
+          `It is ${data["utc_datetime"]} in ${prepareString(
+            args[1]
+          )}/${prepareString(args[0])}.`
+        );
+    }
+    process.exit();
+  });
 }
-
-getTimeData(args[0], args[1]).then(data => {
-  switch (args[2]) {
-    case "--unix":
-      return console.log(
-        `Current unix timestamp for ${prepareString(args[1])}/${prepareString(
-          args[0]
-        )} is ${data["unixtime"]}`
-      );
-    case "--currentWeek":
-      return console.log(
-        `It is week ${data["week_number"]} in ${prepareString(
-          args[1]
-        )}/${prepareString(args[0])}.`
-      );
-    case "--dayOfTheYear":
-      return console.log(
-        `It is day ${data["day_of_year"]} of the year in ${prepareString(
-          args[1]
-        )}/${prepareString(args[0])}.`
-      );
-    default:
-      return console.log(
-        `It is ${data["utc_datetime"]} in ${prepareString(
-          args[1]
-        )}/${prepareString(args[0])}.`
-      );
-  }
-  process.exit();
-});
--- messaging.js
+++ messaging.js
@@ -1,7 +1,8 @@
+const { name, version } = require("./package.json");
 exports.showHelp = () => {
   // handle input
   // no input to see here
-  const helpText = "Here the help text appears";
+  const helpText = `Thank you for using ${name}`;
   // print output
   console.log(helpText);
 };
